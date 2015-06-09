27b1622712a11a45b5c95024c0328bf9451bc715
hugoabonizio <hugoseriesmanager@gmail.com>
2015-06-09 03:59:30 +0000

    Add logger to debug production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+	
+	config.logger = Logger.new(STDOUT)
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
